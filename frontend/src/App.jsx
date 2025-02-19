b35f3435aa87385c23517070cc43cb3e463e4a8a
shubhamdhobale2021 <shubhamdhobale2021.it@mmcoe.edu.in>
2025-02-19 23:59:34 +0530

    Create routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import {Routes , Route , BrowserRouter} from 'react-router-dom'
+import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold'>Trade-Mitra</div>
+    <div className=''>
+      <BrowserRouter>
+      <Header/> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/signin' element={<SignIn/>} />
+        <Route path='/signup' element={<SignUp/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
