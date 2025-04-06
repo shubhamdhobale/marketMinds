d16890a84f55a319a94b39f69a9ccb1b14580a22
shubhamdhobale2021 <shubhamdhobale2021.it@mmcoe.edu.in>
2025-04-06 16:03:57 +0530

    Updated Team Information

--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -1,9 +1,9 @@
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import './Team.css';
 import { motion } from "framer-motion";
-import founderImg from '../../public/assets/founder-removebg-preview.png'
-import founderImg2 from '../../public/assets/omkar.png'
-import founderImg3 from '../../public/assets/rohan.png'
+import founderImg from '/assets/founder-removebg-preview.png'
+import founderImg2 from '/assets/omkar.png'
+import founderImg3 from '/assets/rohan.png'
 
 
 const Team = () => {
@@ -16,13 +16,16 @@ const Team = () => {
           <img src={founderImg} className="absolute bottom-30 w-40 drop-shadow-[0_5px_10px_rgba(0,0,0,0.45)] transition-transform duration-500 hover:scale-105 hover:drop-shadow-[0_8px_25px_rgba(0,0,0,0.45)] mask-image [mask-image:linear-gradient(to_bottom,rgba(0,0,0,1),rgba(0,0,0,0))]"/>
           <div className=' relative top-12'>
               <h4 className="text-xl font-bold text-gray-800">Shubham Dhobale</h4>
-              <p className="text-gray-600 mb-4">Founder & CEO</p>
+              <p className="text-gray-600 mb-4">Founder & Lead Developer</p>
               <div className="flex justify-center gap-4">
                 <a href="https://www.linkedin.com/in/shubham-dhobale-114083255/" target="_blank" rel="noopener noreferrer">
                   <FaLinkedin size={24} className="text-[#0077B5] hover:text-[#005582]" />
                 </a>
                 <a href="https://github.com/shubhamdhobale" target="_blank" rel="noopener noreferrer">
                   <FaGithub size={24} className="text-black hover:text-gray-700" />
+                </a>
+                <a href="https://www.instagram.com/_shubham.dhobale?igsh=NWhmbzJvbm92enU5" target="_blank" rel="noopener noreferrer">
+                  <FaInstagram size={24} className="text-black hover:text-gray-700" />
                 </a>
           </div>
               </div>
@@ -33,11 +36,14 @@ const Team = () => {
               <h4 className="text-xl font-bold text-gray-800">Omkar Bhagat</h4>
               <p className="text-gray-600 mb-4">Manager</p>
               <div className="flex justify-center gap-4">
-                <a href="" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/omkar-bhagat-91b61a289/" target="_blank" rel="noopener noreferrer">
                   <FaLinkedin size={24} className="text-[#0077B5] hover:text-[#005582]" />
                 </a>
                 <a href="" target="_blank" rel="noopener noreferrer">
                   <FaGithub size={24} className="text-black hover:text-gray-700" />
+                </a>
+                <a href="https://www.instagram.com/omkarbhagat_89?igsh=amZxNmdiZTdqOTFm" target="_blank" rel="noopener noreferrer">
+                  <FaInstagram size={24} className="text-black hover:text-gray-700" />
                 </a>
               </div>
           </div>
@@ -49,11 +55,14 @@ const Team = () => {
               <h4 className="text-xl font-bold text-gray-800">Rohan Khaire</h4>
               <p className="text-gray-600 mb-4">Marketing</p>
               <div className="flex justify-center gap-4">
-                <a href="" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/rohan-khaire-1621362a5/" target="_blank" rel="noopener noreferrer">
                   <FaLinkedin size={24} className="text-[#0077B5] hover:text-[#005582]" />
                 </a>
                 <a href="" target="_blank" rel="noopener noreferrer">
                   <FaGithub size={24} className="text-black hover:text-gray-700" />
+                </a>
+                <a href="https://www.instagram.com/rohann_khaire?igsh=eG04Y2t2N2ExeXFm" target="_blank" rel="noopener noreferrer">
+                  <FaInstagram size={24} className="text-black hover:text-gray-700" />
                 </a>
               </div>
           </div>
