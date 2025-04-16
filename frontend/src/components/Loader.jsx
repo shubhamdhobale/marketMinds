23f8437f149c29bde66c519b32f63c3a02dc0075
shubhamdhobale2021 <shubhamdhobale2021.it@mmcoe.edu.in>
2025-04-16 14:53:59 +0530

    Loader component

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,12 +1,9 @@
-import Layout from "./Layout.jsx";
 
 const Loader = () => {
   return (
-    <Layout>
-      <div className="flex justify-center items-center h-screen bg-white mt-10 z-50">
+    <div className="flex justify-center items-center h-screen mt-10 z-50">
         <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-opacity-75"></div>
-      </div>
-    </Layout>
+    </div>
   );
 };
 
