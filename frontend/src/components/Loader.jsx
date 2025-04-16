504734c1a58811ea6e56c5488771dcc4f2560758
shubhamdhobale2021 <shubhamdhobale2021.it@mmcoe.edu.in>
2025-04-16 16:19:38 +0530

    Loader component

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,14 +1,32 @@
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen mt-10 z-50">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-opacity-75"></div>
-      <div className="border border-gray-600 rounded-lg p-6">
-        <div className="h-6 bg-gray-700 rounded w-3/4 mx-auto"></div>
-        <div className="h-4 bg-gray-700 rounded w-1/2 mx-auto"></div>
-        <div className="h-48 bg-gray-800 rounded w-full mt-6"></div>        
+    <div className="flex flex-col justify-center items-center min-h-screen z-50 bg-white">
+      {/* Animation Container */}
+      <div className="flex gap-4 pt-10 pb-6">
+        <div className="flex flex-col items-center animate-[bounce_1s_ease-in-out_infinite_0.1s]">
+          <div className="w-1 h-6 bg-green-500"></div>
+          <div className="w-3 h-12 bg-green-500 rounded-sm"></div>
+          <div className="w-1 h-6 bg-green-500"></div>
+        </div>
+
+        <div className="flex flex-col items-center animate-[bounce_1s_ease-in-out_infinite_0.2s]">
+          <div className="w-1 h-6 bg-red-500"></div>
+          <div className="w-3 h-12 bg-red-500 rounded-sm"></div>
+          <div className="w-1 h-6 bg-red-500"></div>
+        </div>
+
+        <div className="flex flex-col items-center animate-[bounce_1s_ease-in-out_infinite_0.1s]">
+          <div className="w-1 h-6 bg-green-500"></div>
+          <div className="w-3 h-12 bg-green-500 rounded-sm"></div>
+          <div className="w-1 h-6 bg-green-500"></div>
+        </div>
       </div>
+
+      {/* Text Below Animation */}
+      <p className="text-black text-2xl font-bold mt-2">Loading...</p>
     </div>
+
   );
 };
 
